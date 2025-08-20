4da3e842df179ff800212fa082470d7ba955799b
Onkareshwar Pal <onkareshwar.pal@ibm.com>
2025-08-20 10:32:54 +0530

    Added RabiitMq in User services

--- user_services/src/app.module.ts
+++ user_services/src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { UserController } from './user/user.controller';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from '../prisma/prisma.module';
 
